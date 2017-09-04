df216204e0d06012c0a317af3f1ea98b4ea62acf
imaarthi <aarthi_anbalagan@brown.edu>
2017-09-04 15:33:31 -0400

    Server side

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
-from flask import Flask
+from flask import Flask, redirect
+from flask import request
 import sys
+import json
+import csv
 
 sys.dont_write_bytecode = True
-
+with open('./test.csv', 'w') as csv_file:
+	writer = csv.writer(csv_file, delimiter=',')
+	writer.writerow(['numClicksMemphis'])
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -10,9 +15,13 @@ app.config['DEBUG'] = True
 # Simple python Flask server
 
 @app.route('/')
-def admin():
-	return "OK"
+def root():
+	redirect('main.html')
 
 @app.route('/data')
-def admin():
-	return "OK"
+def get_data():
+
+	data = json.loads(request.data)
+	n_clicks = data.get('numClicksMemphis')
+
+	writer.writerow(n_clicks)
