f60975da5ba6f2c2a6c7f2d2f9a9c71dda9e7efb
imaarthi <aarthi.cit@gmail.com>
2017-09-08 20:21:01 -0400

    added procfile

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template
+from flask import request
+import sys
+import random
+import csv
+
+sys.dont_write_bytecode = True
+with open('./ab_test.csv', 'w') as csv_file:
+    writer = csv.writer(csv_file, delimiter=',')
+    writer.writerow(['version','pageLoadTime', 'clickTime'])
+
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+
+# Simple python Flask server
+
+@app.route('/', methods=['GET'])
+def root():
+    probability = random.uniform(0, 1)
+    if probability < 0.5:
+        return render_template('A.html')
+    else:
+        return render_template('B.html')
+
+
+@app.route('/data', methods=['GET', 'POST'])
+def get_data():
+    json = request.get_json()
+    version = json['version']
+    clickTime = json['clickTime']
+    pageLoadTime = json['pageLoadTime']
+    with open('./ab_test.csv', 'a') as csv_file:
+        writer = csv.writer(csv_file, delimiter=',')
+        writer.writerow([version, pageLoadTime, clickTime])
+    return "OK"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
